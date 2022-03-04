8e9b9d6a1f40672f8c550ad3723ac84f367b56df
Panos Mavrogiorgos <pmav99@gmail.com>
2022-03-04 11:01:34 +0200

    Create environment

--- setup_miniconda.sh
+++ setup_miniconda.sh
@@ -7,11 +7,18 @@ log() {
   echo "$(date +'%T.%3N') - $1"
 }
 
+environment_name='schism'
 
-log 'Installing miniconda'
+log 'Installing miniconda: Start'
 mv .condarc ~/
 wget -O ~/miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash ~/miniconda.sh -b -p ~/miniconda
 eval "$(~/miniconda/condabin/conda shell.bash hook)"
 conda init
 log 'Installing miniconda: Done'
+
+log 'Creating environment: Start'
+conda create --yes --name "${environment_name}" --file conda.lock
+conda activate "${environment_name}"
+which schism
+log 'Creating environment: Done'
